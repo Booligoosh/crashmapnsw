689aca9848c4e2ef41cd1a958c8c3f8bf09e9aba
Ethan <me@ethan.link>
2023-02-12 22:05:48 +1100

    Separate main/vendor/data chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        // https://rollupjs.org/configuration-options/#output-manualchunks
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) return "vendor";
+          if (id.includes("src/data/")) return "data";
+        },
+      },
+    },
+  },
+});
